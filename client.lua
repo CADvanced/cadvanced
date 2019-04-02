a52266c8dd81edb6d9bb0f2a5ee0709f9161345c
Andrew Isherwood <github@warmlight.co.uk>
2019-04-02 19:59:06 +0100

    Fix call to GetPlayerPed

--- client.lua
+++ client.lua
@@ -2,7 +2,7 @@ local oldPos
 
 Citizen.CreateThread(function()
     while true do
-        local pos = GetEntityCoords(GetPlayerPed())
+        local pos = GetEntityCoords(GetPlayerPed(-1))
         if (oldPos ~= pos)then
             TriggerServerEvent('cv:updatePosition', pos.x, pos.y, pos.z)
             oldPos = pos
