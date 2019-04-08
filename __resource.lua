755036c06f8b96fed7554f56493b9fc11b8fc79c
Andrew Isherwood <github@warmlight.co.uk>
2019-04-08 20:26:31 +0100

    WIP: More work...

--- __resource.lua
+++ __resource.lua
@@ -1,4 +1,13 @@
 description 'Cadvanced'
 
+ui_page 'html/index.html'
+
 server_script 'server.lua'
 client_script 'client.lua'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/reset.css',
+--    'html/listener.js'
+}
--- client.lua
+++ client.lua
@@ -1,5 +1,8 @@
 local oldPos
 
+local calls = {}
+local units = {}
+
 Citizen.CreateThread(function()
     while true do
         local pos = GetEntityCoords(GetPlayerPed(-1))
@@ -9,4 +12,17 @@ Citizen.CreateThread(function()
         end
         Citizen.Wait(5000)
     end
+end)
+
+RegisterNetEvent('msg:updateMsg')
+AddEventHandler('msg:updateMsg', function(message)
+    -- Change to
+    -- https://forum.fivem.net/t/switching-from-chatmessage-to-chat-addmessage/373482
+    TriggerEvent('chatMessage', "", {255, 255, 255}, message)
+end)
+
+RegisterNetEvent('player:addedToUnit');
+AddEventHandler('player:addedToUnit', function(unit)
+    table.insert(calls, unit)
+    print(calls)
 end)
